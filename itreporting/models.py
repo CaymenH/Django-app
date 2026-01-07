1dfe2c97576eb30bb54e9ea9e4337da8e4b9e35b
CaymenH <Caymenhayes1@icloud.com>
2026-01-07 15:09:40 +0000

    added 3 models Student,Module and Registration

--- itreporting/models.py
+++ itreporting/models.py
@@ -5,13 +5,11 @@ from django.urls import reverse
 
 
 class Issue(models.Model):
-    type = models.CharField(max_length=100, choices = [('Hardware',
-'Hardware'), ('Software', 'Software')])
+    type = models.CharField(max_length=100, choices = [('Hardware','Hardware'), ('Software', 'Software')])
     room = models.CharField(max_length=100)
     urgent = models.BooleanField(default = False)
     details = models.TextField()
     date_submitted = models.DateTimeField(default=timezone.now)
-    description = models.TextField()
     author = models.ForeignKey(User, related_name = 'issues',
     on_delete=models.CASCADE)
     def __str__(self):
@@ -21,3 +19,23 @@ class Issue(models.Model):
         {'pk': self.pk})
     
 
+class Module(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+    credit = models.IntegerField()
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    availability = models.CharField (max_length=100, choices= [('Available', 'Available' ), ('Unavailable','Unavailable')])
+    register = models.CharField(max_length=50)
+
+
+class Registration(models.Model):
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+    date_registered = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['student', 'module']
+    
+    def __str__(self):
+        return f'Registration #{self.id}'
--- users/models.py
+++ users/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
-
-
-class Profile(models.Model):
+class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Date_of_birth = models.DateField(null=True, blank=True)
+    Address= models.CharField(max_length=255, blank=True)
+    City_town = models.CharField(max_length=255, blank=True)
+    Country = models.CharField(max_length=255, blank=True)
     image = models.ImageField(default='media/profile_pics/default.png',
     upload_to='profile_pics')
-def __str__(self):
-    return f'{self.user.first_name} {self.user.last_name}'
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
